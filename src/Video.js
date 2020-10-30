39f94f019cbefeb61c7f506364398d2c7d6d3305
Josh <joshuscurtis@gmail.com>
2020-10-30 19:34:28 +0100

    embedded videos

--- src/Video.js
+++ src/Video.js
@@ -2,8 +2,12 @@ import React from "react";
 import "./styles.css";
 
 export default function Video(props) {
+	
+	var vidSrc = "https://www.youtube.com/embed/" + props.data.snippet.resourceId.videoId;
+	
   return (
     <div className="Video">
+	        <iframe title={vidSrc} width="25%" height="200px" src={vidSrc}></iframe>
       <a
         href={
           "https://www.youtube.com/watch?v=" +

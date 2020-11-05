0e933cc9f12762e0f0371b16564c5ab444f17e92
joshuscurtis <joshuscurtis@gmail.com>
2020-11-05 16:41:29 +0100

    fix

--- src/Video.js
+++ src/Video.js
@@ -2,29 +2,6 @@ import React from "react";
 import "./styles.css";
 
 export default function Video(props) {
-  const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-  ];
-  const days = ["null", "1st", "2nd", "3rd"];
-  var date = new Date(
-    props.data.contentDetails.videoPublishedAt.substring(0, 10)
-  );
-  if (date.getDay() < 4) {
-    var longDate = days[date.getDay()] + " " + monthNames[date.getMonth()];
-  } else {
-    longDate = date.getDay() + "th " + monthNames[date.getMonth()];
-  }
   return (
     <div className="Video">
       <a
@@ -33,7 +10,7 @@ export default function Video(props) {
           props.data.snippet.resourceId.videoId
         }
       >
-        <h1>{longDate}</h1>
+        <h1>{props.data.snippet.title.split("|")[2]}</h1>
         <h2>{props.data.snippet.title.split("|")[0]}</h2>
         <h3>{props.data.snippet.title.split("|")[1]}</h3>
       </a>
